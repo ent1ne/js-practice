5db78df2ac8ffe85a7ac792827004267b34a43d3
Valentin Chernov <qa.valentinchernov@gmail.com>
2023-05-15 23:16:59 +0600

    Removed unnecessary function nesting, added the blank line to the script end

--- Rock, paper, scissors/README.md
+++ Rock, paper, scissors/README.md
@@ -1,14 +1,6 @@
 # Rock, paper, scissors
 This is my first JS project — a simple "Rock, paper, scissors" game for DevTools console
-Let's get you through it block by block, bloke
 
-First, I wrote the function that will be responsible for the computer's choice in every round
-Each time machine randomly chooses rock, paper or scissors in its' eternal struggle against humanity
+Each time the game randomly decides computer's choice and asks you to choose and print "rock", "paper" or "scissors" 
 
-Next I've defined variables responsible for both choices in every round
-There is also some error handling added for the player input
-Both choices made case-insensitive and lead to uniformity by .toLowerCase() method
-
-Then I wrote a function that will cover the entire round of the game
-
-And finally, we find the winner and print the result of completed round into DevTools console
+Then it compares choices made by you and computer and finds the winner, or declares a tie
--- Rock, paper, scissors/script.js
+++ Rock, paper, scissors/script.js
@@ -1,39 +1,39 @@
 function main() {
   let playerSelection = getPlayerSelection()
   let computerSelection = getComputerSelection();
-
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `Computer chose ${playerSelection} too. It's a tie!`;
-    } else if (playerSelection === "rock" && computerSelection === "scissors" || 
-               playerSelection === "paper" && computerSelection === "rock" ||
-               playerSelection === "scissors" && computerSelection === "paper") {
-        return `Computer chose ${computerSelection} \nYou win, ${playerSelection} beats ${computerSelection}`;
-    } else { 
-        return `Computer chose ${computerSelection} \nYou lose, ${computerSelection} beats ${playerSelection}`;
-    }
-  }
-
   const roundResult = playRound(playerSelection, computerSelection);
   console.log(roundResult);
 }
 
 function getPlayerSelection() {
-    let playerChoice;
-    while (true) {
-      playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
-      if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        return playerChoice;
-      } else {
-        alert("Wrong choice. Please enter 'rock', 'paper', or 'scissors'.");
-      }
+  let playerChoice;
+  while (true) {
+    playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+      return playerChoice;
+    } else {
+      alert("Wrong choice. Please enter 'rock', 'paper', or 'scissors'.");
     }
   }
+}
 
 function getComputerSelection() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `Computer chose ${playerSelection} too. It's a tie!`;
+  } else if (
+  playerSelection === "rock" && computerSelection === "scissors" || 
+  playerSelection === "paper" && computerSelection === "rock" ||
+  playerSelection === "scissors" && computerSelection === "paper") {
+    return `Computer chose ${computerSelection} \nYou win, ${playerSelection} beats ${computerSelection}`;
+  } else { 
+    return `Computer chose ${computerSelection} \nYou lose, ${computerSelection} beats ${playerSelection}`;
+  }
 }
 
 main()
